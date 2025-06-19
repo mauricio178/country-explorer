bfdda55065c377e1bb9cccd4674b50c9a390fe6c
mauricio-martins <mauricio.martins@seasolutions.io>
2025-06-19 00:31:19 -0300

    create makefile

---
+++ makefile.win.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Para o container
+docker stop country-explorer-frontend-container
+
+:: Remove o container
+docker rm -f  country-explorer-frontend-container
+
+:: Remove a imagem
+docker rmi country-explorer-frontend
+
+:: Build da nova imagem
+docker build -t country-explorer-frontend --no-cache .
+
+:: Inicia o container
+docker run --name country-explorer-frontend-container ^
+           -e NEXT_PUBLIC_API_URL="https://restcountries.com/v3.1" ^
+           -p 3000:3000 country-explorer-frontend
+
+pause
