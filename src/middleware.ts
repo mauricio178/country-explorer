97bcfaa2b7272944cde3231ea8060bfe86169455
mauricio-martins <mauricio.martins@seasolutions.io>
2025-06-19 13:44:33 -0300

    Uncomment middleware - build fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,17 @@
-// Using the next.config.ts file to redirect the root path to the home path
-// This file is not used anymore
-// But is valid for future references and complex redirects
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { systemPaths } from "@/constants/paths";
 
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import { systemPaths } from "@/constants/paths";
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
 
-// export function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL(systemPaths.home, request.url));
+  }
 
-//   if (pathname === "/") {
-//     return NextResponse.redirect(new URL(systemPaths.home, request.url));
-//   }
+  return NextResponse.next();
+}
 
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/"],
-// };
+export const config = {
+  matcher: ["/"],
+};
